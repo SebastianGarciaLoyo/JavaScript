c16f5849bfb22d4dcaf3e2bdf79754d2c7e95019
SebastianGarciaLoyo <145623552+SebastianGarciaLoyo@users.noreply.github.com>
2023-11-29 21:39:58 -0500

    Add files via upload

---
+++ 1.Introduccion/miscript.js
@@ -0,0 +1,15 @@
+console.log("         *  *****")
+console.log("         * *     *")
+console.log("         * *")
+console.log("         *  *****")
+console.log("  *      *       *")
+console.log("  *      *       *")
+console.log("   *****    *****")
+
+alert("yuli es una tonta")
+
+body = document.querySelector("body")
+
+document.write("<h2>Probando</h2>")
+
+body.innerHTML += "<h2> hola</h2>"
