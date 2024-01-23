cd95328e71f20a490a66d748c437541ea491b6db
SebastianGarciaLoyo <est.sloyo@smart.edu.co>
2024-01-23 02:47:15 +0000

    nuevo

--- Filtro JavaScript/js/funcionario.js
+++ Filtro JavaScript/js/funcionario.js
@@ -1,33 +1,26 @@
-import { controlador } from "../controllers/controlador.js";
-export { get } from "../models/get.js"
 
+function busca(){
+    const div = document.querySelector("#resultado")
+    const p = document.createElement("table")
+    p.innerHTML = `
+                <br>
+                <div id="respon">
+                <h2>Datos Usuario</h2>
+                <p>Nombre:</p>
+                <p>ID:</p>
+                <p>Contra:</p>
+                <p>Compras Restantes para el bono: </p>
+                <br>
+                <button onclick="cierre()">Cerrar</button>
+                </div>
+                `
+    div.append(p)
 
-document.getElementById("buscar").addEventListener("submit",function(event){
-    event.preventDefault();
+}  
 
-    fetch('http://localhost:4001/usuarios')
-    .then(response => response.json())
-    .then(usuariosregistrados => {
-        const encontrar = usuariosregistrados.find(usuario => usuario.id, usuario.pass, usuarios.nombre)
-        if (encontrar == true){
-            function busca(){
-                const div = document.querySelector("#resultado")
-                const p = document.createElement("table")
-                p.innerHTML = `
-                            <br>
-                            <div id="respon">
-                            <h2>Datos Usuario</h2>
-                            <p>Nombre:${nombre} </p>
-                            <p>ID: ${id}</p>
-                            <p>Contra ${pass}</p>
-                            <p>Compras Restantes para el bono: </p>
-                            </div>
-                            `
-                div.append(p)
-            
-            }  
-        }
-        
-    })
-    
-})
+function cierre(){
+    const div  = document.querySelector("#resultado")
+    const text = document.querySelector("#respon")
+    text.innerHTML = ""
+    div.insertBefore(text, null)
+}
